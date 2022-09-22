be248345b3ebc798a4026379eec210498bdc1537
James Nock <jpnock@gmail.com>
2022-09-22 01:11:23 +0100

    tools: Add MacOS install script

---
+++ tools/darwin.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo () {
+    printf "\n%b\n" "[iac] $1"
+}
+
+echo "Running brew update"
+brew update
+
+echo "Installing Verilator"
+brew install verilator@4.214
+
+echo "Adding RISC-V toolchain repo"
+brew tap riscv-software-src/riscv
+
+echo "Installing RISC-V toolchain"
+brew install riscv-tools
+
+echo "Installing additional deps..."
+brew install python
+python3 -m ensurepip
+
+brew install gcc make cmake bison \
+    flex bc autoconf \
+    gperf ccache qemu gdb
