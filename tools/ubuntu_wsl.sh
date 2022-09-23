3c91c91adcf2dc6402dafb8518ee919b7ccd6131
James Nock <jpnock@gmail.com>
2022-09-23 02:41:57 +0100

    tools: Improve script comment

--- tools/ubuntu_wsl.sh
+++ tools/ubuntu_wsl.sh
@@ -11,7 +11,7 @@ gzip_code=0
 echo "Testing gzip"
 gzip 2>/dev/null || gzip_code=$?
 if [ "${gzip_code}" -eq 126 ]; then
-    echo "Broken gzip detected; applying patch..."
+    echo "Broken gzip detected; applying patch... you may need to enter your password..."
     echo -en '\x10' | sudo dd of=/usr/bin/gzip count=1 bs=1 conv=notrunc seek=$((0x189))
 else
     echo "gzip working correctly"
