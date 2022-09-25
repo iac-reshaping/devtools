a4e4412dbc5cf6d50e685550c6a4cfe96dd39886
James Nock <jpnock@gmail.com>
2022-09-25 22:31:31 +0100

    tools: Improve install of riscv toolchain for x86 MacOS

--- tools/darwin.sh
+++ tools/darwin.sh
@@ -29,43 +29,16 @@ brew install make cmake \
 
 echo "Installing RISC-V toolchain"
 cd "${IAC_FOLDER}"
-if [[ $(uname -m) == 'arm64' ]]; then
-    echo "[Detected Platform] Apple Silicon"
-    rm -rf riscv-gnu-toolchain.tar.gz
-    curl --output riscv-gnu-toolchain.tar.gz -L https://github.com/iac-reshaping/devtools/releases/download/v1.0.0-rc.1/riscv-gnu-toolchain-2022-09-21-apple-silicon.tar.gz
-    sudo tar -xzf riscv-gnu-toolchain.tar.gz --directory /opt
-    export PATH="/opt/riscv/bin:$PATH"
-    # shellcheck disable=SC2016
-    printf '\n%s' 'export PATH="/opt/riscv/bin:$PATH"' >> ~/.zprofile
-else
-    echo "[Detected Platform] x86-64 Apple"
-    brew install python3 gawk gnu-sed gmp mpfr libmpc isl zlib expat
-    brew tap discoteq/discoteq
-    brew install flock
 
-    toolchain_git_folder="${IAC_FOLDER}/riscv-gnu-toolchain"
-    
-    if [ ! -d "${toolchain_git_folder}" ] 
-    then
-        echo "Cloning the riscv-gnu-toolchain repository... this may take a long time..."
-        git clone https://github.com/riscv/riscv-gnu-toolchain "${toolchain_git_folder}" &>/dev/null
-        cd "${toolchain_git_folder}"
-        git checkout 2022.09.21
-    else 
-        echo "Updating the riscv-gnu-toolchain repository"
-        cd "${toolchain_git_folder}"
-        git fetch
-        git checkout 2022.09.21
-        git status
-    fi
-
-    sudo mkdir -p /opt/riscv
-    export PATH="/opt/riscv/bin:$PATH"
-    # shellcheck disable=SC2016
-    printf '\n%s' 'export PATH="/opt/riscv/bin:$PATH"' >> ~/.zprofile
-    ./configure --prefix=/opt/riscv --enable-multilib
-    sudo make
-fi
+arch_name=$(uname -m)
+rm -rf riscv-gnu-toolchain.tar.gz
+curl --output riscv-gnu-toolchain.tar.gz -L "https://github.com/iac-reshaping/devtools/releases/download/v1.0.0-rc.1/riscv-gnu-toolchain-2022-09-21-Darwin-${arch_name}.tar.gz"
+sudo rm -rf /opt/riscv
+sudo tar -xzf riscv-gnu-toolchain.tar.gz --directory /opt
+export PATH="/opt/riscv/bin:$PATH"
+# shellcheck disable=SC2016
+printf '\n%s' 'export PATH="/opt/riscv/bin:$PATH"' >> ~/.zprofile
+rm -rf riscv-gnu-toolchain.tar.gz
 
 echo "riscv-gnu-toolchain installed..."
 riscv64-unknown-elf-gcc --version
