e471b2acfca778dc4da93a89944a66a92b616801
James Nock <jpnock@gmail.com>
2022-09-25 15:18:44 +0100

    tools: Improve post-install script

--- tools/install.sh
+++ tools/install.sh
@@ -102,7 +102,11 @@ else
     "$POST_RUN_SCRIPT"
 fi
 
-echo "Installation complete..."
-echo "Make sure vscode is installed, then to get started run: code ${TOOLS_FOLDER}/autumn/workspace/iac-autumn.code-workspace"
-echo "Make sure to mark the workspace as trusted & install the recommended extensions when prompted"
-code "${TOOLS_FOLDER}/autumn/workspace/iac-autumn.code-workspace"
+echo "IAC Devtools Installation complete..."
+echo "Make sure VS Code is installed then run the following. Make sure to mark the workspace as trusted and install the recommended extensions when prompted"
+printf "\n================\n%s\n================" "code ${TOOLS_FOLDER}/autumn/workspace/iac-autumn.code-workspace"
+if ! which code &>/dev/null; then
+    echo "VS Code does not appear to be installed..."
+else
+    code "${TOOLS_FOLDER}/autumn/workspace/iac-autumn.code-workspace"
+fi
